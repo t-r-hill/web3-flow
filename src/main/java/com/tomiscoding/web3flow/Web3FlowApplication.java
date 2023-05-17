df71bf17e3b2d31641b797b82d4841dcf311c5c4
Thomas Hill <tomhill555@gmail.com>
2023-05-17 16:35:31 +0100

    adding spring boot init

---
+++ src/main/java/com/tomiscoding/web3flow/Web3FlowApplication.java
@@ -0,0 +1,13 @@
+package com.tomiscoding.web3flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web3FlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web3FlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomiscoding/web3flow/Web3FlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomiscoding.web3flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web3FlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
